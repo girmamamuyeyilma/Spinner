1eaeaf0dfc16bae9cb020d9e322d8b901c6becc2
girma.yilma <girmamamuye.yilma@studenti.unitn.it>
2016-05-20 10:48:25 +0200

    Comments added for describing the code

--- app/src/main/java/com/spinner/girmayilma/spinner/MainActivity.java
+++ app/src/main/java/com/spinner/girmayilma/spinner/MainActivity.java
@@ -22,22 +22,24 @@ import android.widget.Spinner;
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
-   // EditText mEditText;
-    String[] stringArray = { "Amharic", "English", "Italian", "German", "Greece" };
+    String[] stringArray = { "Amharic", "English", "Italian", "German", "Greece" };// String array to be adapted for spinner
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        preventSoftKeyboardDisplayExceptEditText(findViewById(R.id.parent));
+        preventSoftKeyboardDisplayExceptEditText(findViewById(R.id.parent));//parent id refers to the id of the main activity layout
+        /*comment the above line if you don't want
+         to hide the keyboard for all instance of other view groups*/
 
-        spinner = (Spinner) findViewById(R.id.spinner);
-        //mEditText = (EditText) findViewById(R.id.edit_text);
+        spinner = (Spinner) findViewById(R.id.spinner);//get spinner
         ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item,
                                                                            stringArray);
-        spinner.setAdapter(stringArrayAdapter);
+        spinner.setAdapter(stringArrayAdapter);//adapt the spinner
 
-  /*     spinner.setOnTouchListener(new View.OnTouchListener() {
+        /* remove the block comment line below to prevent soft keyboard while the spinner view gets focused*/
+
+  /*  spinner.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -48,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*function to hide keyboard*/
     public static void hideSoftKeyboard(View v) {
         if (v!=null) {
             InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -55,19 +58,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*function to get focused view from activity*/
+    private View getFocusedView() {
+        View mWindow = (View) getCurrentFocus();
+        return mWindow!=null ? mWindow : null;
 
-   private View getFocusedView(){
-       View mWindow = (View) getCurrentFocus();
-       return mWindow != null ? mWindow : null;
+    }
 
-   }
-
-
-
+    /*Function to hide the soft keyboard except EditText instance*/
     public void preventSoftKeyboardDisplayExceptEditText(View view) {
 
         //Set up touch listener for non-text box views to hide keyboard.
-        if(!(view instanceof EditText)) {
+        if (!(view instanceof EditText)) {
 
             view.setOnTouchListener(new View.OnTouchListener() {
 
@@ -82,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         //If a layout container, iterate over children and seed recursion.
         if (view instanceof ViewGroup) {
 
-            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+            for (int i = 0; i<((ViewGroup) view).getChildCount(); i++) {
 
                 View innerView = ((ViewGroup) view).getChildAt(i);
 
